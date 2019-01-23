2d106123bf961e84bdc2c9d30dface254de7bc73
Daniel Bok <daniel.bok@outlook.com>
2019-01-23 10:20:52 +0800

    Added convenient subclasses for gaussian and student

--- copulae/elliptical/__init__.py
+++ copulae/elliptical/__init__.py
@@ -1,7 +1,13 @@
 from .gaussian import GaussianCopula
 from .student import StudentCopula
 
-NormalCopula = GaussianCopula
-TCopula = StudentCopula
+
+class NormalCopula(GaussianCopula):
+    pass
+
+
+class TCopula(StudentCopula):
+    pass
+
 
 __all__ = ['NormalCopula', 'GaussianCopula', 'TCopula', 'StudentCopula']
