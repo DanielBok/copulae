fa8f599b0138d15d754325f15215fed43ab0609b
Daniel Bok <daniel.bok@outlook.com>
2019-02-04 00:39:10 +0800

    Refactored Array type

--- copulae/types.py
+++ copulae/types.py
@@ -1,6 +1,7 @@
-from typing import Iterable, List, Union, Optional
+from typing import Iterable, Union, Optional
+
 import numpy as np
 
-Array = Union[List[float], np.ndarray]
+Array = Union[Iterable[float], np.ndarray]
 Numeric = Union[np.ndarray, Iterable, int, float]
 OptNumeric = Optional[Numeric]
