ceff224aaca1244888e7b3fea743bd2c01478daf
Daniel Bok <daniel.bok@outlook.com>
2019-03-03 14:42:35 +0800

    Added polyn_eval (horner scheme polynomial evaluation) and tests

--- copulae/types.py
+++ copulae/types.py
@@ -2,6 +2,6 @@ from typing import Iterable, Union, Optional
 
 import numpy as np
 
-Array = Union[Iterable[float], np.ndarray]
+Array = Union[Iterable[float], Iterable[int], np.ndarray]
 Numeric = Union[np.ndarray, Iterable[Union[float, int]], int, float]
 OptNumeric = Optional[Numeric]
