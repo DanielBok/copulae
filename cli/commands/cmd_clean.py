97f88a8ccc119579febe94e16fc48d14b6468354
Daniel Bok <daniel.bok@outlook.com>
2019-03-08 15:30:49 +0800

    Added cli command for docs

--- cli/commands/cmd_clean.py
+++ cli/commands/cmd_clean.py
@@ -9,6 +9,22 @@ from cli.utils import ROOT, echo, style
 @click.command()
 def cli():
     """Removes unnecessary artifacts after build and tests"""
+    deleted_items = clean()
+    if deleted_items is None:
+        return 1
+
+    echo('Deleted', style(len(deleted_items), 'red'), 'items')
+    for i, (typ, name) in enumerate(deleted_items):
+        echo('\t',
+             style(f'{i + 1:2d}', 'green'),
+             f': {name} ',
+             style(f'[{typ}]', 'yellow' if typ == 'Folder' else 'blue'),
+             sep='')
+    return 0
+
+
+def clean():
+    echo("Cleaning up")
 
     # files and folders to destroy
     to_delete = [
@@ -35,15 +51,7 @@ def cli():
                     shutil.rmtree(i)
                     deleted_items.append(('Folder', i))
 
-        echo('Deleted', style(len(deleted_items), 'red'), 'items')
-        for i, (typ, name) in enumerate(deleted_items):
-            echo('\t',
-                 style(f'{i + 1:2d}', 'green'),
-                 f': {name} ',
-                 style(f'[{typ}]', 'yellow' if typ == 'Folder' else 'blue'),
-                 sep='')
-
-        return 0
+        return deleted_items
     except FileNotFoundError as e:
         echo(str(e), err=True)
-        return 1
+        return None
