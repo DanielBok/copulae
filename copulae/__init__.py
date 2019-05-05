166ef8f8def79f7252fb4c0d76c372d64fe08404
Daniel Bok <daniel.bok@outlook.com>
2019-05-05 14:19:08 +0800

    Added __version__ and docs in the package __init__.py

--- copulae/__init__.py
+++ copulae/__init__.py
@@ -1,3 +1,12 @@
+from copulae._version import get_versions
 from copulae.archimedean import *
 from copulae.elliptical import *
 from copulae.indep import *
+
+__version__ = get_versions()['version']
+del get_versions
+
+
+def doc():
+    import webbrowser
+    webbrowser.open('https://copulae.readthedocs.io/en/latest')
