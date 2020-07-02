428f2959aff1a18311b8abefc63c905955b125b8
Daniel Bok <daniel.bok@outlook.com>
2020-07-02 23:46:47 +0800

    Fixed some bugs GaussianMixtureCopula and added Summary for it

---
+++ copulae/mixtures/gmc/summary.py
@@ -0,0 +1,53 @@
+import pandas as pd
+
+from .parameter import GMCParam
+
+
+class Summary:
+    def __init__(self, params: GMCParam, fit_details: dict):
+        self.name = "Gaussian Mixture Copula"
+        self.params = params
+        self.fit = fit_details
+
+    def as_html(self):
+        return self._repr_html_()
+
+    def _repr_html_(self):
+        params = [f"<strong>{title}</strong>" + pd.DataFrame(values).to_html(header=False, index=False)
+                  for title, values in [("Mixture Probability", self.params.prob),
+                                        ("Means", self.params.means),
+                                        ("Covariance", self.params.covs)]]
+
+        fit_details = ''
+        if self.fit['method'] is not None:
+            fit_details = f"""
+<div>
+    <h3>Fit Details</h3>
+    <div>Algorithm: {self.fit['method']}</div>
+</div>
+            """.strip()
+
+        html = f"""
+<div>
+    <h2>{self.name} Summary</h2>
+    <div>{self.name} with {self.params.n_clusters} components and {self.params.n_dim} dimensions</div>
+    <hr/>
+    <div>
+        <h3>Parameters</h3>
+        {'<br/>'.join(params)}
+    </div>
+    {fit_details}
+</div>
+""".strip()
+
+        return html
+
+    def __repr__(self):
+        return str(self)
+
+    def __str__(self):
+        return '\n'.join([
+            f"{self.name} Summary",
+            "=" * 80,
+            str(self.params)
+        ])
